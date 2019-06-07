7aa035e33eaa65090815dbbbd34108130518ed48
littleknightgames <38872872+littleknightgames@users.noreply.github.com>
2019-06-07 13:16:29 -0400

    Made Tab and Content tabs

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React, { Component } from "react";
+import Card from "./Card";
 
-const Cards = props => {
-  return (
-    <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
-    </div>
-  )
+class Cards extends React.Component {
+  makeCardHandler = () => {
+    console.log(this.props.filter);
+    if (this.props.cards) {
+      return this.props.cards.map(card => {
+        if (card.tab === this.props.filter || this.props.filter === "") {
+          return <Card card={card} />;
+        }
+      });
+    }
+  };
+  render() {
+    return <div className="cards-container">{this.makeCardHandler()}</div>;
+  }
 }
-
 // Make sure you include prop types for all of your incoming props
 
 export default Cards;
